f89cb2886081e4b4c389c82cd2a1ba1075a61203
mkaski <msjkas@utu.fi>
2015-06-02 22:55:20 +0300

    sound service added

--- public/ajastin.js
+++ public/ajastin.js
@@ -24,20 +24,24 @@ app.config([
     }]);
 
 // Home controller
-app.controller("HomeCtrl", function ($scope) {
-    $scope.message = "message";
+app.controller("HomeCtrl", function ($scope, Sound) {
+
+    $scope.sound = Sound;
 });
 
 // Countdown controller
-app.controller("CountdownCtrl", function ($scope, $timeout, $stateParams) {
+app.controller("CountdownCtrl", function ($scope, $timeout, $stateParams, Sound) {
 
     // minutes from url parameter
-    var minutes = $stateParams.minutes;
+    var minutes = Math.ceil($stateParams.minutes * 100) / 100;
     // make total seconds
     var seconds = minutes * 60;
 
     // paused state
     $scope.paused = false;
+
+    // sound on/off
+    $scope.sound = Sound;
 
     // countdown function, count down if seconds > 0 and counter is not paused
     $scope.countdown = function () {
@@ -78,11 +82,42 @@ app.controller("CountdownCtrl", function ($scope, $timeout, $stateParams) {
         }
     };
 
+    $scope.soundToggle = function() {
+        $scope.sound = !sound;
+    };
+
     // time is up
     var timeUp = function() {
+
+        if ($scope.sound.getSound()) {
+            Sound.playAlarm();
+        }
         alert("Aika loppu!");
     };
 
     // start timer on page load
     $timeout($scope.countdown, 1000);
+});
+
+// Sound service
+app.service("Sound", function Sound() {
+
+    var Sound = this;
+    var sound = true;
+    var alarm = document.createElement('audio');
+    alarm.setAttribute('src', 'alert.ogg');
+
+    Sound.setSound = function() {
+        sound = !sound;
+    }
+
+    Sound.getSound = function() {
+        return sound;
+    }
+
+    // play alarm
+    Sound.playAlarm = function() {
+        alarm.play();
+    };
+
 });
